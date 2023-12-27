99d49f3a0ac6891406975671193bac673e8dd59b
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-12-26 22:01:00 -0300

    Finalizando página de busca de todos os veiculos

--- src/views/ListarVeiculos.vue
+++ src/views/ListarVeiculos.vue
@@ -3,43 +3,52 @@
     <ion-content>
       <h3 align="center">Listagem de veículos cadastrados</h3>
       
-      <ion-card v-if="dadosRecebidos">
-        <ion-card-header v-for="carro in dadosRecebidos" :key="carro.id" class="card">
-          <ion-card-title>{{carro.modelo}}</ion-card-title>
-          <ion-card-subtitle>Cor: {{carro.cor}}</ion-card-subtitle>
-          <ion-card-subtitle>Ano: {{carro.ano}}</ion-card-subtitle>
-          <ion-card-subtitle><strong>Placa: {{carro.placa}}</strong></ion-card-subtitle>
-        </ion-card-header>
-      </ion-card>
+      <ion-refresher slot="fixed" @ionRefresh="handleRefresh($event)">
+        <ion-refresher-content>
+        </ion-refresher-content>
+      </ion-refresher>
+          <ion-card v-if="dadosRecebidos">
+            <ion-card-header v-for="carro in dadosRecebidos" :key="carro.id" class="card">
+              <ion-card-title>{{carro.modelo}}</ion-card-title>
+              <ion-card-subtitle>Cor: {{carro.cor}}</ion-card-subtitle>
+              <ion-card-subtitle>Ano: {{carro.ano}}</ion-card-subtitle>
+              <ion-card-subtitle><strong>Placa: {{carro.placa}}</strong></ion-card-subtitle>
+            </ion-card-header>
+          </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
   import axios from 'axios'
-  import { IonPage, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonRefresher} from '@ionic/vue';
-  import { defineComponent } from 'vue';
+  import { IonPage, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonRefresher, IonRefresherContent} from '@ionic/vue';
+  import { defineComponent, ref, onMounted } from 'vue';
 
   export default defineComponent({
-    components: { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonPage, IonContent, IonRefresher},
-    data() {
-      return {
-        dadosRecebidos: null,
-      };
-    },
-    mounted() {
-      this.realizarRequisicao();
-    },
-    methods: {
-      async realizarRequisicao() {
+    components: { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonPage, IonContent, IonRefresher, IonRefresherContent},
+
+    setup() {
+      const dadosRecebidos = ref(null);
+
+      const realizarRequisicao = async () => {
         try {
           const response = await axios.get('http://localhost/api-veiculos-laravel/public/api/veiculos');
-          
-          this.dadosRecebidos = response.data;
+          dadosRecebidos.value = response.data;
         } catch (error) {
           console.error('Erro na requisição GET:', error);
         }
-      },
+      };
+
+      onMounted(() => {
+        realizarRequisicao();
+      });
+
+      const handleRefresh = async (event: CustomEvent) => {
+        await realizarRequisicao();
+        event.detail.complete();
+      };
+
+      return { dadosRecebidos, handleRefresh };
     },
   });
 </script>
